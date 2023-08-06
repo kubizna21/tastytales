9b93cab1d9624634b2bfb658f891158d052aed49
Fadi-Faiyad <108450748+Fadi-Faiyad@users.noreply.github.com>
2023-08-06 13:14:46 -0700

    Add files via upload

---
+++ src/componemts/Veggie.jsx
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react";
+import Styled from "styled-components";
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css/skyblue'
+import { Link } from "react-router-dom";
+
+function Veggie() {
+  const [veggie, setVeggie] = useState([])
+
+
+  useEffect(() => {
+    getVeggie();
+  }, []);
+
+  const getVeggie = async () => {
+
+    // the code here well help with filltering the api
+    const check = localStorage.getItem('veggie');
+
+// this checks if the data is saved to local storge
+    if (check) {
+      setVeggie(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegetarian`
+      )
+      const data = await api.json();
+
+      localStorage.setItem('veggie', JSON.stringify(data.recipes));
+      console.log(data)
+      setVeggie(data.recipes)
+    };
+  }
+  return (
+    <div>
+    <Wraped>
+      <h3>Vegetarin Picks</h3>
+      <Splide options={{
+        perPage: 3,
+        arrows: false,
+        pagination: false,
+        drag: 'free',
+        gap: '5rem',
+      }}>
+        {veggie.map((recipe) => {
+          return (
+            <SplideSlide key={recipe.id}>
+              <Sheet>
+                <Link to={'/recipe/' + recipe.id}>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt="{recipe.title}" />
+                <Gradient />
+                </Link>
+              </Sheet>
+            </SplideSlide>
+          );
+        })
+
+        }
+      </Splide>
+    </Wraped>
+  </div>
+  )
+}
+const Wraped = Styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Sheet = Styled.div`
+  min-height: 25rem; /* Fixed typo: Changed min-hight to min-height */
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+const Gradient = Styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+
+
+
+`;
+
+
+export default Veggie
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Veggie from "../componemts/Veggie"
+import Populer from "../componemts/Populer"
+
+
+function Home() {
+  return (
+    <div> 
+        <Veggie />
+        <Populer />
+    </div>
+  )
+  }
+export default Home;
